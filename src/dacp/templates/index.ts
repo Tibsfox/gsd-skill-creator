d4c8a63cc3669135a063abed4922130944e1a9b0
Tibsfox <tibsfox@tibsfox.com>
2026-02-27 01:58:01 -0800

    feat(452-01): implement template registry with CRUD and persistence

---
+++ src/dacp/templates/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Public barrel export for the DACP templates module.
+ *
+ * @module dacp/templates
+ */
+
+export { BundleTemplateRegistry } from './registry.js';
+export type { TemplateRegistryPort, BundleTemplate } from './types.js';
