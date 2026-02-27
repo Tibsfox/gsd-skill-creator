ae7839dd8ee893c3bcd2ce4952a55ab4de00246d
Tibsfox <tibsfox@tibsfox.com>
2026-02-27 05:29:20 -0800

    feat(fs): implement ProjectManager with init/list/status

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,6 +1,7 @@
 export { loadConfig, saveConfig, defaultConfig, getConfigPath, resolveProjectPath, CONFIG_FILENAME } from "./config.js";
 export type { ScConfig } from "./config.js";
 export { scaffoldZones } from "./scaffold.js";
+export { ProjectManager } from "./project-manager.js";
 export type {
   Zone,
   ProjectDescriptor,
